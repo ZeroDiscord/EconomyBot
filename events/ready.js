e6ae1908afe096fba1b742be48d75ed0e14560c5
ZeroDiscord <68364573+ZeroDiscord@users.noreply.github.com>
2020-10-12 13:03:54 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,16 +1,4 @@
 module.exports = (client) => {
     console.log(`${client.user.tag} is online!`);
-    setInterval(() => {
-        const statusArray = client.config.games;
-        const randomStatus = Math.floor(Math.random() * statusArray.length);
-        const status = statusArray[randomStatus];
-        client.user.setActivity(
-            status.name
-            .replace("{prefix}", client.config.prefix)
-            .replace("{users}", client.users.cache.size.toLocaleString())
-            .replace("{servers}", client.guilds.cache.size.toLocaleString()),
-            { 
-                type: status.type 
-            });
-    }, 7000);
+  client.user.setActivity("Coded by ZeroSync");
 };
