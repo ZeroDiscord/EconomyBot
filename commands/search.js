0013d720c0a396ace2fb571c9e57dbca56af8931
ZeroDiscord <68364573+ZeroDiscord@users.noreply.github.com>
2020-10-13 12:53:11 +0530

    Update search.js

--- commands/search.js
+++ commands/search.js
@@ -1,18 +1,18 @@
-module.exports.execute = async (client, message, args) => {
+exports.execute = async (client, message, args) => {
     let users = [
         "Pocket",
         "T-Shirt",
-        "Bed",
+        "Zero's Databse",
         "Street"
     ];
     let amount = Math.floor(Math.random() * 200) + 50;
     let beg = await client.eco.beg(client.ecoAddUser, amount, { canLose: true, cooldown: 300000, customName: "search" });
     if (beg.onCooldown) return message.reply(`Come back after ${beg.time.minutes} minutes & ${beg.time.seconds} seconds.`);
     if (beg.lost) return message.channel.send(`**${users[Math.floor(Math.random() * users.length)]}:** You were caught! You couldn't get money kiddo.`);
-    else return message.reply(`**${users[Math.floor(Math.random() * users.length)]}** donated you **${beg.amount}** 💸. Now you have **${beg.after}** 💸.`);
+    else return message.reply(`**${users[Math.floor(Math.random() * users.length)]}** was somewhat profitable, you found **${beg.amount}** 💸. Now you have **${beg.after}** 💸.`);
 };
 
-module.exports.help = {
+exports.help = {
     name: "search",
     aliases: [],
     usage: "search"
