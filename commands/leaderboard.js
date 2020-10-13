34b79412f05348315587e403890afa76a784077e
ZeroDiscord <68364573+ZeroDiscord@users.noreply.github.com>
2020-10-13 13:11:48 +0530

    v12 update

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -7,10 +7,10 @@ exports.execute = async (client, message, args) => {
     const embed = new MessageEmbed()
         .setAuthor(`Leaderboard of ${message.guild.name}!`, message.guild.iconURL)
         .setColor("RANDOM")
-        .setThumbnail(client.users.cache.get(leaderboard[0].id) ? client.users.get(leaderboard[0].id).displayAvatarURL : "https://cdn.discordapp.com/avatars/603948445362946084/a_f61398e073d78ae104e32b0517c891c3.gif")
+        .setThumbnail(client.users.cache.get(leaderboard[0].id) ? client.users.cache.get(leaderboard[0].id).displayAvatarURL : "https://cdn.discordapp.com/avatars/603948445362946084/a_f61398e073d78ae104e32b0517c891c3.gif")
         .setTimestamp();
     leaderboard.forEach(u => {
-        embed.addField(`${u.position}. ${client.users.cache.get(u.id) ? client.users.get(u.id).tag : "Unknown#0000"}`, `${u.money} 💸`);
+        embed.addField(`${u.position}. ${client.users.cache.get(u.id) ? client.users.cache.get(u.id).tag : "Unknown#0000"}`, `${u.money} 💸`);
     });
     return message.channel.send(embed);
 }
