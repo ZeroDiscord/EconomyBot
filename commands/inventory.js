d0e7b42ee416f5aaf3cd7d3789ac3b7eb884dde8
ZeroDiscord <68364573+ZeroDiscord@users.noreply.github.com>
2021-04-24 17:36:17 +0530

    Added Item Quantities

--- commands/inventory.js
+++ commands/inventory.js
@@ -1,19 +1,25 @@
-const { MessageEmbed } = require("discord.js")
+cconst { MessageEmbed } = require("discord.js")
 exports.execute = async (client, message, args) => {
+
+  const embed = new MessageEmbed()
+    .setAuthor(`Inventory of ${message.author.tag}`, message.guild.iconURL)
+    .setColor("RANDOM")
+    .setThumbnail()
+    .setTimestamp();
+  const x = client.db.get(`items_${message.author.id}`);
+
+const arrayToObject = x.reduce((itemsobj, x) => {
+    itemsobj[x.name] = (itemsobj[x.name] || 0) + 1;
+    return itemsobj;
+}, {});
+const result = Object.keys(arrayToObject).map(k => embed.addField(`Name: ${k}`,`Quantity: **${arrayToObject[k]}**`, false));
+  
  
-    const embed = new MessageEmbed()
-        .setAuthor(`Inventory of ${message.author.tag}`, message.guild.iconURL)
-        .setColor("RANDOM")
-        .setThumbnail()
-        .setTimestamp();
-        const x = client.db.get(`items_${message.author.id}`);
-    x.forEach(u => {
-        embed.addField(`Name : ${u.name}.`, `Price : **${u.prize}**`);
-    });
-    return message.channel.send(embed);
+  return message.channel.send(embed);
 }
 exports.help = {
-    name: "inventory",
-    aliases: ["inv"],
-    usage: `inv`
+  name: "inventory",
+  aliases: ["inv"],
+  usage: `inv`
 }
+
