3633fd256a303360fc9e351df698db4d3f8543d7
BEGINSCOIDNG <86099011+BEGINSCOIDNG@users.noreply.github.com>
2021-06-18 12:57:33 +0530

    Update botConfig.js

--- botConfig.js
+++ botConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
-    token: "YOUR_TOKEN",
-    prefix: "PREFIX",
+    token: "ODU1MzQ3OTAzNjM0NjA0MDUy.YMxKzw.5uj0Cvg_uMyk5hOynb6JnTxc5Yw",
+    prefix: "e!",
     admins: [
         "PEOPLE WHO CAN USE ADD MONEY (IDS)"
 ],
